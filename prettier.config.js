d444d20e690220a68996cd3a281dc6b79e02217b
0xnekoya <nekoya.bc@gmail.com>
2024-06-18 11:08:57 +0900

    Add `prettier.config.js`

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+export default {
+  plugins: [
+    'prettier-plugin-organize-imports',
+    'prettier-plugin-packagejson',
+    'prettier-plugin-tailwindcss',
+  ],
+  semi: false,
+  endOfLine: 'lf',
+  singleQuote: true,
+  jsxSingleQuote: true,
+}
